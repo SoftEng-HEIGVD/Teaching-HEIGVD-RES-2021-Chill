7db37ba2f4b79ac9a84af83d0c6fa3147112356a
Alessandro <alessandro.parrinoap@gmail.com>
2021-02-26 17:30:13 +0100

    Limoncello liqueur

---
+++ src/test/java/ch/heigvd/res/chill/domain/Alessandro_AP/LimoncelloTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.Alessandro_AP;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.domain.wasadigi.Boxer;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LimoncelloTest {
+
+    @Test
+    void thePriceAndNameForBoxerShouldBeCorrect() {
+        Limoncello limo = new Limoncello();
+        assertEquals(limo.getName(), Limoncello.NAME);
+        assertEquals(limo.getPrice(), Limoncello.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForBoxer() {
+        Bartender alex = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.Alessandro_AP.Limoncello";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = alex.order(request);
+        BigDecimal expectedTotalPrice = Limoncello.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+}

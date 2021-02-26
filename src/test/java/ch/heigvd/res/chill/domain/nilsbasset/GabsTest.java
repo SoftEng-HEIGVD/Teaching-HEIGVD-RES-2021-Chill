2ae65417380c8a7ee0c1a2aff5e02536adc706e8
nilsbasset <49392726+nilsbasset@users.noreply.github.com>
2021-02-26 15:40:22 +0100

    new beer Gabs

---
+++ src/test/java/ch/heigvd/res/chill/domain/nilsbasset/GabsTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.nilsbasset;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GabsTest {
+
+  @Test
+  void thePriceAndNameForBoxerShouldBeCorrect() {
+    Gabs beer = new Gabs();
+    assertEquals(beer.getName(), Gabs.NAME);
+    assertEquals(beer.getPrice(), Gabs.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForBoxer() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.res.chill.domain.nilsbasset.Gabs";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = Gabs.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}

f8d251be1ee025f8315a021967fcaf6d985dc938
Rotho1998 <rob.gaudin@hotmail.com>
2021-02-26 15:14:38 +0100

    Ajout propre fichiers

---
+++ src/test/java/ch/heigvd/res/chill/domain/robin_gaudin/DesperadosTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.robin_gaudin;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DesperadosTest {
+
+  @Test
+  void thePriceAndNameForDesperadosShouldBeCorrect() {
+    Desperados beer = new Desperados();
+    assertEquals(beer.getName(), Desperados.NAME);
+    assertEquals(beer.getPrice(), Desperados.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForDesperados() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.res.chill.domain.robin_gaudin.Desperados";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = Desperados.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}

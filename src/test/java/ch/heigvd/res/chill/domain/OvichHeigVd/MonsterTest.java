c8842d15af72184fbc4552df7c8836e3c3dbd8d6
Ovich <stefanteofanovic@hotmail.com>
2021-02-26 11:24:20 +0100

    Include Monster in product list. Fix issues with RedBull test package

--- src/test/java/ch/heigvd/res/chill/domain/OvichHeigVd/MonsterTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/OvichHeigVd/MonsterTest.java
@@ -1,8 +1,9 @@
-package ch.heigvd.res.chill.domain.wasadigi;
+package ch.heigvd.res.chill.domain.OvichHeigVd;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.domain.OvichHeigVd.*;
 import ch.heigvd.res.chill.protocol.OrderRequest;
 import ch.heigvd.res.chill.protocol.OrderResponse;
 import org.junit.jupiter.api.Test;
@@ -11,21 +12,21 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class RedBullTest {
+class MonsterTest {
     @Test
-    void thePriceAndNameForRedbullShouldBeCorrect(){
-        RedBull drink = new RedBull();
-        assertEquals(drink.getName(), RedBull.NAME);
-        assertEquals(drink.getPrice(), RedBull.PRICE);
+    void thePriceAndNameForMonsterShouldBeCorrect(){
+        Monster drink = new Monster();
+        assertEquals(drink.getName(), Monster.NAME);
+        assertEquals(drink.getPrice(), Monster.PRICE);
     }
 
     @Test
-    void aBartenderShouldAcceptAnOrderForRedbull() {
+    void aBartenderShouldAcceptAnOrderForMonster() {
         Bartender jane = new Bartender();
-        String productName = "ch.heigvd.res.chill.domain.wasadigi.RedBull";
+        String productName = "ch.heigvd.res.chill.domain.OvichHeigVd.Monster";
         OrderRequest request = new OrderRequest(3, productName);
         OrderResponse response = jane.order(request);
-        BigDecimal expectedTotalPrice = RedBull.PRICE.multiply(new BigDecimal(3));
+        BigDecimal expectedTotalPrice = Monster.PRICE.multiply(new BigDecimal(3));
         assertEquals(expectedTotalPrice, response.getTotalPrice());
     }
 }

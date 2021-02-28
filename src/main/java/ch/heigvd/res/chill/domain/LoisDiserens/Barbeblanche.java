90050798cd8e56153072d596df3fde18bfcc8715
LoisDiserens <lois.diserens@hispeed.ch>
2021-02-28 11:33:18 +0100

    Ajout de la Frimousse - Barbeblanche

--- src/main/java/ch/heigvd/res/chill/domain/LoisDiserens/Barbeblanche.java
+++ src/main/java/ch/heigvd/res/chill/domain/LoisDiserens/Barbeblanche.java
@@ -1,4 +1,24 @@
-package ch.heigvd.res.chill.domain;
+package ch.heigvd.res.chill.domain.LoisDiserens;
 
-public class Barbeblanche {
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Barbeblanche implements IProduct
+{
+
+    public final static String NAME = "Frimousse - Barbeblanche";
+    public final static BigDecimal PRICE = new BigDecimal(3.0); // Prix Chillout
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice()
+    {
+        return PRICE;
+    }
 }
--- src/test/java/ch/heigvd/res/chill/domain/LoisDiserens/BarbeblancheTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/LoisDiserens/BarbeblancheTest.java
@@ -1,4 +1,32 @@
+package ch.heigvd.res.chill.domain.LoisDiserens;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
-class BarbeblancheTest {
-  
+
+class NebuleuseTest {
+
+    @Test
+    void PrixEtNomCorrectFinDuMonde() {
+        Barbeblanche beer = new Barbeblanche();
+        assertEquals(beer.getName(), Barbeblanche.NAME);
+        assertEquals(beer.getPrice(), Barbeblanche.PRICE);
+    }
+
+    @Test
+    void AccepterCommandePourFinDuMonde() {
+        int nbBieresCommandees = 3;
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.LoisDiserens.Barbeblanche";
+        OrderRequest request = new OrderRequest(nbBieresCommandees, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Barbeblanche.PRICE.multiply(new BigDecimal(nbBieresCommandees));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
 }

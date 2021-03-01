d04c9b5f2fe223945ae705730c3f2dd5c96dc4c8
nilsbasset <49392726+nilsbasset@users.noreply.github.com>
2021-03-01 11:04:49 +0100

    Correction methode test Gabs + add Coca

--- src/test/java/ch/heigvd/res/chill/domain/nilsbasset/CocaTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/nilsbasset/CocaTest.java
@@ -9,22 +9,22 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GabsTest {
+class CocaTest {
 
   @Test
-  void thePriceAndNameForGabsShouldBeCorrect() {
-    Gabs beer = new Gabs();
-    assertEquals(beer.getName(), Gabs.NAME);
-    assertEquals(beer.getPrice(), Gabs.PRICE);
+  void thePriceAndNameForCocaShouldBeCorrect() {
+    Coca beer = new Coca();
+    assertEquals(beer.getName(), Coca.NAME);
+    assertEquals(beer.getPrice(), Coca.PRICE);
   }
 
   @Test
-  void aBartenderShouldAcceptAnOrderForGabs() {
+  void aBartenderShouldAcceptAnOrderForCoca() {
     Bartender jane = new Bartender();
-    String productName = "ch.heigvd.res.chill.domain.nilsbasset.Gabs";
+    String productName = "ch.heigvd.res.chill.domain.nilsbasset.Coca";
     OrderRequest request = new OrderRequest(3, productName);
     OrderResponse response = jane.order(request);
-    BigDecimal expectedTotalPrice = Gabs.PRICE.multiply(new BigDecimal(3));
+    BigDecimal expectedTotalPrice = Coca.PRICE.multiply(new BigDecimal(3));
     assertEquals(expectedTotalPrice, response.getTotalPrice());
   }
 

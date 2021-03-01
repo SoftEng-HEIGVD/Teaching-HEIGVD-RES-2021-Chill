f6ff0e2ccd586b33d83fc2b8b69de8f40c85fb7f
LoisDiserens <lois.diserens@hispeed.ch>
2021-03-01 13:22:44 +0100

    Ajout de la Frimousse - Barbeblanche

--- src/test/java/ch/heigvd/res/chill/domain/LoisDiserens/BarbeblancheTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/LoisDiserens/BarbeblancheTest.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class NebuleuseTest {
+class BarbeblancheTest {
 
     @Test
     void PrixEtNomCorrectFinDuMonde() {
